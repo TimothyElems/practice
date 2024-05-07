3b5938f183a6874fd08ec3a2e57a1138aaecd614
Timothy Elems <85181892+TimothyElems@users.noreply.github.com>
2024-05-07 17:10:33 +0000

    nim hello world

---
+++ basic1/000/nim/main.nim
@@ -0,0 +1,14 @@
+#[
+  # I am going to try all the methods of printing Hello World!
+  # I will start with just echoing it to the command line. DONE
+  # I will try to run it with different variables
+]#
+echo "Hello, World!"
+
+var 
+  hello: string = "Hello,"
+  world: string = "World!"
+
+echo hello, " ", world
+
+
