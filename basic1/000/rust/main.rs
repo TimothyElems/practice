a4cf8b1b43b0945e275a91397261330b30aec7de
Timothy Elems <85181892+TimothyElems@users.noreply.github.com>
2024-05-07 17:47:18 +0000

    rusty

---
+++ basic1/000/rust/main.rs
@@ -0,0 +1,16 @@
+/*
+ * I will try the same process as Python.
+ * First run is to send it to the CLI using print.
+ * Then I'll run it with variables! It all works!
+ */
+
+// println!("Hello, World!");
+// The above code has compile time errors because it lacks a main function
+fn main() {
+    println!("Hello, World!");
+
+    let x: &str = "Hello,";
+    let y: &str = "World!";
+
+    println!("{} {}", x, y);
+}
